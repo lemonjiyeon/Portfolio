baa2118dd6ea98f7dc6ec1a00166cf22dc8e5ff2
lemonjiyeon <govl2411@naver.com>
2022-02-18 10:34:48 +0900

    최종수정

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 <!--
-	Massively by HTML5 UP
+	Stellar by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
@@ -9,219 +9,326 @@
 		<title>JIYEON CHAE - PORTFOLIO</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="./assets/css/main.css" />
+		<link rel="stylesheet" href="./assets/css/main.css?after" />
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+		<script>
+			// bxslider 설정
+			$(document).ready(function(){
+				$('.slider').bxSlider({
+					controls:false
+				});
+			});
+		</script>
 		<noscript><link rel="stylesheet" href="./assets/css/noscript.css" /></noscript>
 	</head>
 	<body class="is-preload">
 
-		<!-- Wrappere -->
-			<div id="wrapper" class="fade-in">
-
-				<!-- Intro -->
-					<div id="intro">
-						<h1>I'm Developer <br /></h1>
-						<h2>CHAE JIYEON</h2>
-						<ul class="actions">
-							<li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
-						</ul>
-					</div>
+		<div id="bgwrapper">
+		<!-- Wrapper -->
+			<div id="wrapper">
 
 				<!-- Header -->
-					<header id="header">
-						<a href="#footer" class="logo">Contact</a>
+					<header id="header" class="alt">
+						<span class="logo"><img class="profile" src="images/profile.jpg" alt="" /></span>
+						<h1 class="profilename">JIYEON CHAE</h1>
+						<div class="typingarea">
+							<div class="typing-txt">
+								<ul class="txt">
+									<li>안녕하세요.</li>
+									<li>저는 사용자 관점의 서비스를 지향하는</li>
+									<li>신입 개발자 채지연입니다.</li>
+								</ul>
+							</div>
+							<div class="typing">
+								<ul class="txt">
+									<li></li>
+									<li></li>
+									<li></li>
+								</ul>
+							</div> 
+						</div>
 					</header>
 
 				<!-- Nav -->
 					<nav id="nav">
-						<ul class="links">
-							<li class="active"><a href="index.html">This is Massively</a></li>
-							<li><a href="generic.html">Generic Page</a></li>
-							<li><a href="elements.html">Elements Reference</a></li>
-						</ul>
-						<ul class="icons">
-							<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-							<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-							<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-							<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+						<ul>
+							<li><a href="#intro" class="active">Introduction</a></li>
+							<li><a href="#first">Skills</a></li>
+							<li><a href="#second">Projects</a></li>
+							<li><a href="#cta">Get Started</a></li>
 						</ul>
 					</nav>
 
 				<!-- Main -->
 					<div id="main">
 
-						<!-- Featured Post -->
-							<article class="post featured">
-								<header class="major">
-									<span class="date">April 25, 2017</span>
-									<h2><a href="#">And this is a<br />
-									massive headline</a></h2>
-									<p>Aenean ornare velit lacus varius enim ullamcorper proin aliquam<br />
-									facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor<br />
-									amet nullam sed etiam veroeros.</p>
-								</header>
-								<a href="#" class="image main"><img src="images/main.jpg" alt="" /></a>
-								<ul class="actions special">
-									<li><a href="#" class="button large">Full Story</a></li>
-								</ul>
-							</article>
+						<!-- Introduction -->
+							<section id="intro" class="main">
+								<div class="spotlight">
+									<div class="content">
+										<header class="major">
+											<h2>About Me</h2>
+										</header>
 
-						<!-- Posts -->
-							<section class="posts">
-								<article>
-									<header>
-										<span class="date">April 24, 2017</span>
-										<h2><a href="#">Sed magna<br />
-										ipsum faucibus</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions special">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
-								<article>
-									<header>
-										<span class="date">April 22, 2017</span>
-										<h2><a href="#">Primis eget<br />
-										imperdiet lorem</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic03.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions special">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
-								<article>
-									<header>
-										<span class="date">April 18, 2017</span>
-										<h2><a href="#">Ante mattis<br />
-										interdum dolor</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic04.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions special">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
-								<article>
-									<header>
-										<span class="date">April 14, 2017</span>
-										<h2><a href="#">Tempus sed<br />
-										nulla imperdiet</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic05.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions special">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
-								<article>
-									<header>
-										<span class="date">April 11, 2017</span>
-										<h2><a href="#">Odio magna<br />
-										sed consectetur</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic06.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions special">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
-								<article>
-									<header>
-										<span class="date">April 7, 2017</span>
-										<h2><a href="#">Augue lorem<br />
-										primis vestibulum</a></h2>
-									</header>
-									<a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
-									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
-									<ul class="actions special">
-										<li><a href="#" class="button">Full Story</a></li>
-									</ul>
-								</article>
+							<!-- about me -->
+							<div class="AboutMe_whole_wrapeer">
+								<div class="aboutme_wholeinner_wrapper">
+									<div class="aboutme_wrapper">
+										<div class="aboutme_inner_wrapper">
+											<div class="aboutme_icon">
+												<span class="icon solid fa-user"></span>
+											</div>
+											<div class="info">
+												<div class="info_title">이름</div>
+												<div>채지연</div>
+											</div>
+										</div>
+									</div>
+									<div class="aboutme_wrapper">
+										<div class="aboutme_inner_wrapper">
+											<div class="aboutme_icon">
+												<span id="abouticon" class="icon solid fa-calendar"></span>
+											</div>
+											<div class="info">
+												<div class="info_title">생년월일</div>
+												<div>96.04.20</div>
+											</div> 
+										</div>
+									</div>
+									<div class="aboutme_wrapper">
+										<div class="aboutme_inner_wrapper">
+											<div class="aboutme_icon">
+												<span class="icon solid fa-map-marked"></span>
+											</div>
+											<div class="info">
+												<div class="info_title">주소지</div>
+												<div>부산광역시 사상구</div>
+											</div>
+										</div>
+									</div>
+									<div class="aboutme_wrapper">
+										<div class="aboutme_inner_wrapper">
+											<div class="aboutme_icon">
+												<span class="icon solid fa-phone"></span>
+											</div>
+											<div class="info">
+												<div class="info_title">연락처</div>
+												<div>010-8299-6001</div>
+											</div>
+										</div>
+									</div>
+									
+									<div class="aboutme_wrapper_1">
+										<div class="aboutme_inner_wrapper">
+											<div class="aboutme_icon">
+												<span class="icon solid fa-trophy"></span>
+											</div>
+											<div class="info">
+												<div class="info_title">자격증</div>
+												<div class="info_text">컴퓨터 활용능력 1급<br>정보처리기사 1급 필기합격(2022년 1회 실기예정)</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+								<!-- connect me -->
+										<ul class="icons">
+											<li><a href="mailto:govl2411@naver.com" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+											<li><a href="https://github.com/lemonjiyeon" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+										</ul>
+									</div>
+								</div>
+							</section>
+						<!-- end introduction -->
+
+						<!-- Skills Section -->
+							<section id="first" class="main special">
+								<header class="major">
+									<h2>Skills</h2>
+								</header>
+								<ul class="features">
+									<li>
+										<span class="icon solid major style1 fa-desktop"></span>
+										<h3>Frontend</h3>
+										<p>Html5, css, dart, Javascript, react, JQuery 를 사용한 경험이 있으며, bootstrap 및 오픈소스를 활용할 수 있습니다.</p>
+									</li>
+									<li>
+										<span class="icon solid major style3 fa-database"></span>
+										<h3>Backend</h3>
+										<p>java, kotlin, python, mysql, oracle, firebase, mongodb 를 사용한 경험이 있으며, DB를 연결하여 조인 쿼리 및 CRUD를 작성 가능합니다.</p>
+									</li>
+									<li>
+										<span class="icon solid major style5 fa-users"></span>
+										<h3>Devops</h3>
+										<p>인턴 당시 SVN을 통해 형상관리를 해보았고, IT교육 참여 당시 Github를 셋팅해 협업하였습니다.</p>
+									</li>
+								</ul>
+								
 							</section>
 
-						<!-- Footer -->
-							<footer>
-								<div class="pagination">
-									<!--<a href="#" class="previous">Prev</a>-->
-									<a href="#" class="page active">1</a>
-									<a href="#" class="page">2</a>
-									<a href="#" class="page">3</a>
-									<span class="extra">&hellip;</span>
-									<a href="#" class="page">8</a>
-									<a href="#" class="page">9</a>
-									<a href="#" class="page">10</a>
-									<a href="#" class="next">Next</a>
-								</div>
-							</footer>
+						<!-- Second Section -->
+							<section id="second" class="main special">
+								<header class="major">
+									<h2>Projects</h2>
+								</header>
+								
+									<div class="box">
+										<p>
+											<div class="image left">
+												<div class="slider">
+													<div>
+														<img src="images/tourrainbow_1.png" alt=""s/>
+													</div>
+													<div>
+														<img src="images/tourrainbow_plan.png" alt="" />
+													</div>
+													<div>
+														<img src="images/tourrainbow_development.png" alt="" />
+													</div>
+												</div>
+											</div>
+												<h3>Tour Rainbow</h3>
+												프로젝트를 기획하고 UI디자인과 게시판 기능 구현을 진행하였습니다.
+												스프링 부트를 이용한 웹 사이트이며, 기본적인
+												MVC 패턴을 바탕으로 로그인, 회원가입, 게시판, 게시글 관리 기능을 구현하였습니다.
+												
+												
+												<ul class="skill-wrap">
+													<li class="skill">java</li>
+													<li class="skill">html</li>
+													<li class="skill">css</li>
+													<li class="skill">javascript</li>
+													<li class="skill">jquery</li>
+													<li class="skill">mysql</li>
+													<li class="skill">github</li>
+												</ul>
+												<ul class="icons">
+													<li><a href="2팀_프로젝트기획서_최종.pptx" download="TOUR RAINBOW" class="icon object alt fa-file-pdf"><span class="label">PDF</span></a></li>
+													<li><a href="https://github.com/lemonjiyeon/teamproject/tree/main/travelweb" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+												</ul>
+										</p>
+									</div>
+
+									<div class="box">
+										<p>
+											<div class="image left">
+												<div class="slider">
+													<div>
+														<img src="images/udonmain.png" alt=""s/>
+													</div>
+													<div>
+														<img src="images/udon.png" alt="" />
+													</div>
+													<div>
+														<img src="images/udon2.png" alt="" />
+													</div>
+												</div>
+											</div>
+											<h3>우동도(우리동네도서관)</h3>
+											프로젝트를 기획하고 파이어베이스와 연동하여 도서 등록, 찜한 도서목록,
+											메인화면 및 내 정보수정 등 세부 기능을 구현하였습니다.
+											
+											<ul class="skill-wrap">
+												<li class="skill">kotlin</li>
+												<li class="skill">firebase</li>
+												<li class="skill">github</li>
+											</ul>
+
+											<ul class="icons">
+												<li><a href="안드로이드 프로젝트_기획서_4조_최종.pptx" download="UDONDO" class="icon object alt fa-file-pdf"><span class="label">PDF</span></a></li>
+												<li><a href="https://github.com/InwooJeong/UdonPro" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+											</ul>
+										</p>
+									</div>
+
+									<div class="box">
+										<p>
+											<div class="image left">
+												<div class="slider">
+													<div>
+														<img src="images/movie_app.png" alt=""s/>
+													</div>
+												</div>
+											</div>
+											<h3>영화앱</h3>
+											React 클론코딩으로 기본적인 Dom사용으로 렌더링을 하였고,
+											깃을 통해 배포하였습니다.
+					
+											
+											<ul class="skill-wrap">
+												<li class="skill">react</li>
+												<li class="skill">html</li>
+												<li class="skill">github</li>
+											</ul>
+											<ul class="icons">
+												<li><a href="https://lemonjiyeon.github.io/movie_app_2020" class="icon solid alt fa-link"><span class="label">Link</span></a></li>
+												<li><a href="https://github.com/lemonjiyeon/movie_app_2020" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+											</ul>
+										</p>
+									</div>
+
+									<div class="box">
+										<p>
+											<div class="image left">
+												<div class="slider">
+													<div>
+														<img src="images/dagumbari.jpg" alt=""/>
+													</div>
+													<div>
+														<img src="images/dagumbari_1.png" alt="" />
+													</div>
+													<div>
+														<img src="images/dagumbari_2.png" alt="" />
+													</div>
+												</div>
+											</div>
+											<h3>다금바리를 찾아서</h3>
+											이미지 딥러닝 모델을 통해 다금바리를 판별하는 어플로,
+											프로젝트 기획 초안을 작성하고, 
+											데이터 수집 및 정제와 안드로이드 UI 및 기능을 구현하였습니다.
+					
+											
+											<ul class="skill-wrap">
+												<li class="skill">python</li>
+												<li class="skill">kotlin</li>
+												<li class="skill">github</li>
+											</ul>
+											<ul class="icons">
+												<li><a href="빅데이터 시각화.pdf" download="다금바리를 찾아서" class="icon object alt fa-file-pdf"><span class="label">PDF</span></a></li>
+											</ul>
+										</p>
+									</div>
+							</section>
+
+						<!-- Get Started -->
+							<section id="cta" class="main special">
+								<header class="major">
+								<h2 class="footercm">Thank you for visit!</h2>
+								</header>
+							
+							</section>
 
 					</div>
 
 				<!-- Footer -->
 					<footer id="footer">
-						<section>
-							<form method="post" action="#">
-								<div class="fields">
-									<div class="field">
-										<label for="name">Name</label>
-										<input type="text" name="name" id="name" />
-									</div>
-									<div class="field">
-										<label for="email">Email</label>
-										<input type="text" name="email" id="email" />
-									</div>
-									<div class="field">
-										<label for="message">Message</label>
-										<textarea name="message" id="message" rows="3"></textarea>
-									</div>
-								</div>
-								<ul class="actions">
-									<li><input type="submit" value="Send Message" /></li>
-								</ul>
-							</form>
-						</section>
-						<section class="split contact">
-							<section class="alt">
-								<h3>Address</h3>
-								<p>1234 Somewhere Road #87257<br />
-								Nashville, TN 00000-0000</p>
-							</section>
-							<section>
-								<h3>Phone</h3>
-								<p><a href="#">(000) 000-0000</a></p>
-							</section>
-							<section>
-								<h3>Email</h3>
-								<p><a href="#">info@untitled.tld</a></p>
-							</section>
-							<section>
-								<h3>Social</h3>
-								<ul class="icons alt">
-									<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
-									<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
-									<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
-									<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
-								</ul>
-							</section>
-						</section>
+						<p class="copyright">&copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
 					</footer>
 
-				<!-- Copyright -->
-					<div id="copyright">
-						<ul><li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
-					</div>
-
 			</div>
+		</div>
 
 		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
+			<!-- <script src="assets/js/jquery.min.js"></script> -->
 			<script src="assets/js/jquery.scrollex.min.js"></script>
 			<script src="assets/js/jquery.scrolly.min.js"></script>
 			<script src="assets/js/browser.min.js"></script>
 			<script src="assets/js/breakpoints.min.js"></script>
 			<script src="assets/js/util.js"></script>
 			<script src="assets/js/main.js"></script>
-
+			<script src="assets/js/typing.js"></script>
 	</body>
 </html>
