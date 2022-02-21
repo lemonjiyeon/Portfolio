4c1d223a08c95bfae6bdc6a8088de247b0817c96
lemonjiyeon <govl2411@naver.com>
2022-02-21 18:10:38 +0900

    사이즈수정

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
 
 
 				// Movie app slider option
-				$('.slider_1').show().bxSlider({
+				$('.slider_1').bxSlider({
 					controls: false,
 					auto: true,
 					speed: 1000,
@@ -207,6 +207,7 @@
 								<!-- connect me -->
 										<ul class="icons">
 											<li><a href="mailto:govl2411@naver.com" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+											<li><a href="https://github.com/lemonjiyeon" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
 										</ul>
 									</div>
 								</div>
@@ -276,6 +277,7 @@
 												</ul>
 												<ul class="icons">
 													<li><a href="2팀_프로젝트기획서_최종.pptx" download="TOUR RAINBOW" class="icon object alt fa-file-pdf"><span class="label">PDF</span></a></li>
+													<li><a href="https://github.com/lemonjiyeon/teamproject/tree/main/travelweb" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
 												</ul>
 										</p>
 									</div>
@@ -307,6 +309,7 @@
 
 											<ul class="icons">
 												<li><a href="안드로이드 프로젝트_기획서_4조_최종.pptx" download="UDONDO" class="icon object alt fa-file-pdf"><span class="label">PDF</span></a></li>
+												<li><a href="https://github.com/InwooJeong/UdonPro" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
 											</ul>
 										</p>
 									</div>
@@ -332,6 +335,7 @@
 											</ul>
 											<ul class="icons">
 												<li><a href="https://lemonjiyeon.github.io/movie_app_2020" class="icon solid alt fa-link"><span class="label">Link</span></a></li>
+												<li><a href="https://github.com/lemonjiyeon/movie_app_2020" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
 											</ul>
 										</p>
 									</div>
